c22ea78a3e232a5424041d3839e697fd8c8fc6aa
Edifica <edifica.institute@gmail.com>
2026-05-22 01:16:59 +0530

    Update number-system.js

--- frontend/js/number-system.js
+++ frontend/js/number-system.js
@@ -1220,7 +1220,7 @@ if(fracLen > 0){
 
     formattedCarryRaw.slice(
       0,
-      formattedCarryRaw.length - fracLen - 1
+      formattedCarryRaw.length - fracLen + 1
     )
 
     +
@@ -1230,7 +1230,7 @@ if(fracLen > 0){
     +
 
     formattedCarryRaw.slice(
-      formattedCarryRaw.length - fracLen - 1
+      formattedCarryRaw.length - fracLen + 1
     );
 
 }
