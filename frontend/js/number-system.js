1fc960fb962246ef9dbb6500e7b88b2eb2749b56
Edifica <edifica.institute@gmail.com>
2026-05-22 23:44:49 +0530

    Update number-system.js

--- frontend/js/number-system.js
+++ frontend/js/number-system.js
@@ -2819,7 +2819,7 @@ if(qDigit > 0){
 
   divisionVisual += `${' '.repeat(offset)}${spaced(product)}
 ${' '.repeat(offset)}${'-'.repeat(spaced(product).length)}
-${' '.repeat(offset+2)}${spaced(i < a.length - 1  ? remainder + a[i + 1]  : remainder)}
+${' '.repeat(offset + previousCurrent.length - 1)}${spaced(i < a.length - 1  ? remainder + a[i + 1]  : remainder)}
 `;
 
 
