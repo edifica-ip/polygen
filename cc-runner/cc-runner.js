7d7680678560182c2dd392c54d8e633ce1b13560
Edifica <edifica.institute@gmail.com>
2026-06-17 17:33:28 +0530

    Added Log View

--- cc-runner/cc-runner.js
+++ cc-runner/cc-runner.js
@@ -234,6 +234,13 @@ if (/\.c$/i.test(f.path)) {
   source = GCC_COMPAT + "\n\n" + source;
 }
 
+
+
+console.log("=== GENERATED SOURCE ===");
+console.log(source);
+console.log("========================");
+
+
 await fs.writeFile(
   full,
   source,
